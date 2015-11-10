0b9b9bebfeda6c5fe834d0b9c3eaeed375729f49
Jose Carlos Rodríguez Palacín <cpalacin@syscover.com>
2015-11-10 13:28:13 +0100

    set cronjobs

--- src/database/seeds/CronjobComunikTableSeeder.php
+++ src/database/seeds/CronjobComunikTableSeeder.php
@@ -8,8 +8,8 @@ class CronjobComunikTableSeeder extends Seeder {
     public function run()
     {   
         CronJob::insert([
-            ['name_011' => 'Check to create campaigns',     'package_011' => 3,     'cron_expression_011' => '*/5 * * * *',     'key_011' => '03',  'last_run_011' => 0,    'next_run_011' => 0,    'active_011' => 1],
-            ['name_011' => 'Check to send emails',          'package_011' => 3,     'cron_expression_011' => '*/5 * * * *',     'key_011' => '04',  'last_run_011' => 0,    'next_run_011' => 0,    'active_011' => 1]
+            ['name_011' => 'Check to create campaigns',     'package_011' => 3,     'cron_expression_011' => '*/4 * * * *',     'key_011' => '03',  'last_run_011' => 0,    'next_run_011' => 0,    'active_011' => 1],
+            ['name_011' => 'Check to send emails',          'package_011' => 3,     'cron_expression_011' => '*/2 * * * *',     'key_011' => '04',  'last_run_011' => 0,    'next_run_011' => 0,    'active_011' => 1]
         ]);
     }
 }
