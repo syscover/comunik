4fb712a8fd84c96d0fafd6fb44f7105ebcb6455a
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-03-05 11:47:52 +0100

    update service provider

--- src/Syscover/Comunik/ComunikServiceProvider.php
+++ src/Syscover/Comunik/ComunikServiceProvider.php
@@ -12,33 +12,34 @@ class ComunikServiceProvider extends ServiceProvider
 	public function boot()
 	{
 		// include route.php file
-		include realpath(__DIR__ . '/../../routes.php');
+		if (!$this->app->routesAreCached())
+			require __DIR__ . '/../../routes.php';
 
 		// register views
-		$this->loadViewsFrom(realpath(__DIR__ . '/../../views'), 'comunik');
+		$this->loadViewsFrom(__DIR__ . '/../../views', 'comunik');
 
 		// register translations
-		$this->loadTranslationsFrom(realpath(__DIR__ . '/../../lang'), 'comunik');
+		$this->loadTranslationsFrom(__DIR__ . '/../../lang', 'comunik');
 
 		// register public files
 		$this->publishes([
-			realpath(__DIR__ . '/../../../public') => public_path('/packages/syscover/comunik')
+			__DIR__ . '/../../../public' 					=> public_path('/packages/syscover/comunik')
 		]);
 
 		// register config files
 		$this->publishes([
-			realpath(__DIR__ . '/../../config/comunik.php') => config_path('comunik.php')
+			__DIR__ . '/../../config/comunik.php' 			=> config_path('comunik.php')
 		]);
 
         // register migrations
         $this->publishes([
-            __DIR__.'/../../database/migrations/' => base_path('/database/migrations'),
-			__DIR__.'/../../database/migrations/updates/' => base_path('/database/migrations/updates'),
+            __DIR__ . '/../../database/migrations/' 		=> base_path('/database/migrations'),
+			__DIR__ . '/../../database/migrations/updates/' => base_path('/database/migrations/updates'),
         ], 'migrations');
 
         // register migrations
         $this->publishes([
-            __DIR__.'/../../database/seeds/' => base_path('/database/seeds')
+            __DIR__.'/../../database/seeds/' 				=> base_path('/database/seeds')
         ], 'seeds');
 	}
 
@@ -51,5 +52,4 @@ class ComunikServiceProvider extends ServiceProvider
 	{
         //
 	}
-
 }
