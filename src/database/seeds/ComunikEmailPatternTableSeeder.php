69e2aaee99fcca966bbf606485688323cf6aab25
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-04-13 23:58:26 +0200

    seeds

--- src/database/seeds/ComunikEmailPatternTableSeeder.php
+++ src/database/seeds/ComunikEmailPatternTableSeeder.php
@@ -8,8 +8,15 @@ class ComunikEmailPatternTableSeeder extends Seeder {
     public function run()
     {
         EmailPattern::insert([
-            ['name_049' => 'Undelivered Mail Returned to Sender',       'subject_049' => 'Undelivered Mail Returned to Sender',         'operator_049' => 'and',     'message_049' => 'email account that you tried to reach is disabled',      'action_049' => 2],
-            ['name_049' => 'Delivery Status Notification',              'subject_049' => 'Delivery Status Notification (Failure)',      'operator_049' => 'and',     'message_049' => 'Delivery to the following recipients failed',            'action_049' => 2],
+            ['name_049' => 'Undelivered Mail Returned to Sender',               'subject_049' => 'Undelivered Mail Returned to Sender',                 'operator_049' => 'and',     'message_049' => 'email account that you tried to reach is disabled',                                  'action_049' => 3],
+            ['name_049' => 'Delivery Status Notification',                      'subject_049' => 'Delivery Status Notification (Failure)',              'operator_049' => 'and',     'message_049' => 'Delivery to the following recipients failed',                                        'action_049' => 3],
+            ['name_049' => 'Undelivered Mail Returned to Sender',               'subject_049' => 'Undelivered Mail Returned to Sender',                 'operator_049' => 'and',     'message_049' => 'MAILBOX IS FULL',                                                                    'action_049' => 3],
+            ['name_049' => 'Delivery Status Notification (Failure)',            'subject_049' => 'Delivery Status Notification (Failure)',              'operator_049' => 'and',     'message_049' => 'Unknown address error 550',                                                          'action_049' => 3],
+            ['name_049' => 'Undelivered Mail Returned to Sender',               'subject_049' => 'Undelivered Mail Returned to Sender',                 'operator_049' => 'and',     'message_049' => 'Recipient address rejected: User unknown',                                           'action_049' => 3],
+            ['name_049' => 'Failure notice',                                    'subject_049' => 'failure notice',                                      'operator_049' => 'and',     'message_049' => 'The e-mail message could not be delivered because the user\'s mailfolder is full',   'action_049' => 3],
+            ['name_049' => 'Failure notice',                                    'subject_049' => 'failure notice',                                      'operator_049' => 'and',     'message_049' => 'Remote host said: 451 unable to verify sender',                                      'action_049' => 3],
+            ['name_049' => 'Warning: message',                                  'subject_049' => 'failure notice',                                      'operator_049' => 'and',     'message_049' => 'Delay reason: mailbox is full',                                                      'action_049' => 3],
+            ['name_049' => 'Mail delivery failed: returning message to sender', 'subject_049' => 'Mail delivery failed: returning message to sender',   'operator_049' => 'and',     'message_049' => 'mailbox is full: retry timeout exceeded',                                            'action_049' => 3],
         ]);
     }
 }
