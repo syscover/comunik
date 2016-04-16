873b6b6c4629a9c535e254a8548c6af51fde684a
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-04-16 13:01:59 +0200

    cron job

--- src/database/seeds/ComunikCronjobTableSeeder.php
+++ src/database/seeds/ComunikCronjobTableSeeder.php
@@ -10,7 +10,7 @@ class ComunikCronjobTableSeeder extends Seeder {
         CronJob::insert([
             ['name_011' => 'Check to create campaigns',     'package_011' => 5,     'cron_expression_011' => '*/4 * * * *',     'key_011' => '03',  'last_run_011' => 0,    'next_run_011' => 0,    'active_011' => 1],
             ['name_011' => 'Check to send emails',          'package_011' => 5,     'cron_expression_011' => '*/2 * * * *',     'key_011' => '04',  'last_run_011' => 0,    'next_run_011' => 0,    'active_011' => 1],
-            ['name_011' => 'Check to bounced emails',       'package_011' => 5,     'cron_expression_011' => '*/30 * * * *',    'key_011' => '07',  'last_run_011' => 0,    'next_run_011' => 0,    'active_011' => 1],
+            ['name_011' => 'Check to bounced emails',       'package_011' => 5,     'cron_expression_011' => '*/5 * * * *',     'key_011' => '07',  'last_run_011' => 0,    'next_run_011' => 0,    'active_011' => 1],
         ]);
     }
 }
