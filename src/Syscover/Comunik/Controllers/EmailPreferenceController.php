5e3ad38f5947f9668e015ba80a9bc55131309c06
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-05-23 12:48:23 +0200

    refactor viewParameters

--- src/Syscover/Comunik/Controllers/EmailPreferenceController.php
+++ src/Syscover/Comunik/Controllers/EmailPreferenceController.php
@@ -1,6 +1,7 @@
 <?php namespace Syscover\Comunik\Controllers;
 
 use Syscover\Pulsar\Core\Controller;
+use Illuminate\Http\Request;
 use Syscover\Comunik\Models\Group;
 use Syscover\Pulsar\Models\Preference;
 
@@ -19,6 +20,13 @@ class EmailPreferenceController extends Controller
     protected $model        = Preference::class;
     protected $icon         = 'fa fa-cog';
     protected $objectTrans  = 'preference';
+
+    function __construct(Request $request)
+    {
+        parent::__construct($request);
+
+        $this->viewParameters['cancelButton'] = false;
+    }
 
     public function customIndex($parameters)
     {
